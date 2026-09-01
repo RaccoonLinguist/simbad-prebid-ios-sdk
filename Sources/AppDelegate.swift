3990af46a7fa6c2521e6c1fa684d243874a133a4
RaccoonLinguist <134336253+RaccoonLinguist@users.noreply.github.com>
2026-09-01 20:16:53 +0300

    Update AppDelegate.swift

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -4,8 +4,8 @@ import PrebidMobile
 class AppDelegate: NSObject, UIApplicationDelegate {
 func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
-        // ВАЖНО: Теперь мы вызываем метод у URLSession (а не URLSessionConfiguration)
-        URLSession.enablePrebidJSONFixer()
+        // ВАЖНО: вызываем метод у URLSession
+        URLSession.enablePrebidJSONFixer() //Вызов клиентской части отредактированного SDK
         
         // 1. Указываем ваш Account ID - ВМЕСТО 273000419 НУЖНО УКАЗАТЬ ТОТ, КОТОРЫЙ ВЫ ПОЛУЧИТЕ ОТ МЕНЕДЖЕРА SIMB-AD
         Prebid.shared.prebidServerAccountId = "273000419"
