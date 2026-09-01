d0a7d063621779a196a80cb58e99082ce98a0b3e
RaccoonLinguist <134336253+RaccoonLinguist@users.noreply.github.com>
2026-09-01 20:15:59 +0300

    Update MyApp.swift

--- Sources/MyApp.swift
+++ Sources/MyApp.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 @main
 struct MyApp: App {
-    // Связываем запуск приложения с нашим AppDelegate
+    // Связываем запуск приложения с AppDelegate
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
